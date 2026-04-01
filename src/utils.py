03a538d6c395086799c9c636c2fdc4e156db9a79
Jaydeep Vishwakarma <jaydeep328509@gmail.com>
2026-04-02 00:14:43 +0530

    HyperParameter

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import numpy as np
 import dill
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -18,25 +19,34 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_model(X_train, y_train, X_test, y_test, models):
+def evaluate_model(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
         
-        for i in range(len(list(models))):
-            model = list(models.values())[i]
+        for model_name, model in models.items():
+            para = params[model_name]
             
+            # 1. Find the best parameters using GridSearchCV
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+            
+            # 2. Update the original model with the best parameters found
+            model.set_params(**gs.best_params_)
+            
+            # 3. Fit the original model on the full training data
             model.fit(X_train, y_train)
             
+            # 4. Predict and evaluate
             y_train_pred = model.predict(X_train)
-            
             y_test_pred = model.predict(X_test)
             
             train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = r2_score(y_test, y_test_pred)
             
-            report[list(models.keys())[i]] = test_model_score
+            # 5. Save the score
+            report[model_name] = test_model_score
             
-            return report
+        return report
             
     except Exception as e:
         raise CustomException(e, sys)
